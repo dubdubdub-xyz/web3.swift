02d7b508cea572dd0692e44f438116accf8513de
financialvice <cam@dubdubdub.xyz>
2023-01-31 14:48:39 -0800

    idk just trying things ???

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "web3.swift", targets: ["web3"])
     ],
     dependencies: [
-        .package(name: "BigInt", url: "https://github.com/attaswift/BigInt", from: "5.3.0"),
+        .package(name: "BigInt", url: "https://github.com/attaswift/BigInt", .upToNextMinor(from: "5.3.0")),
         .package(name: "GenericJSON", url: "https://github.com/iwill/generic-json-swift", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", .upToNextMajor(from: "0.6.0")),
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.0.0"),
@@ -26,8 +26,8 @@ let package = Package(
                     .target(name: "keccaktiny"),
                     .target(name: "aes"),
                     .target(name: "Internal_CryptoSwift_PBDKF2"),
-                    "BigInt",
-                    "GenericJSON",
+                    .product(name: "BigInt", package: "BigInt"),
+                    .product(name: "GenericJSON", package: "generic-json-swift"),
                     .product(name: "secp256k1", package: "secp256k1.swift"),
                     .product(name: "WebSocketKit", package: "websocket-kit"),
                     .product(name: "Logging", package: "swift-log")
